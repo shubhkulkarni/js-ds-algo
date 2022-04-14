c50dfd399339f32827579d0cf948391942a7f9f9
Shubham Kulkarni <58355613+shubhkulkarni@users.noreply.github.com>
2022-04-14 11:23:54 +0530

    Update ReverseNumber.js

--- src/ReverseNumber.js
+++ src/ReverseNumber.js
@@ -1,16 +1,19 @@
 const reverse1 = num => {
   let rev = 0;
-  while (num != 0) {
+  while (num !== 0) {
     rev = (rev*10) + (num % 10);
     num = Math.trunc(num/10) ; 
   }
   return rev;
 }
 
-const reverse2 = num => num.toString().split("").reverse().join("");
+const reverse2 = num => {
+  let rev = num.toString().split("").reverse().join("");
+  return parseInt(rev) * Math.sign(num);  
+  };
 
+  console.log(reverse2(-1))
+  
 
-
-console.log(reverse1(-1234567)) 
 
 
